89287b0b567f66b2e4b80a31fd89b2e900bfbc9e
Holberton <flockwood@live.com>
2025-03-25 14:32:57 -0400

    fix names

---
+++ handle_specifier.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * handle_specifier - Processes a single format specifier
+ * @format: The format specifier to handle
+ * @args: the list of arguments for the specifier
+ *
+ * Return: The number of characters printed for the specifier
+ */
+
+int handle_specifier(cosnt char *format, va_list args)
+{
+	int count = 0;
+
+	if (!format)
+		return (-1);
+
+	switch (*format)
+	{
+	case 'c':
+		count += print_c(args);
+		break;
+	case 's':
+		count += print_s(args);
+		break;
+	case 'i':
+	case 'd':
+		count += print_di(args);
+		break;
+	case '%':
+		count += _putchar('%');
+		break;
+	default:
+		count += _putchar('%');
+		count += _putchar(*format);
+		break;
+	}
+	return (count);
+}
