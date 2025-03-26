343f1e4303bb0c1e768ca6100a9846b1a421b8d5
Holberton <flockwood@live.com>
2025-03-26 09:14:34 -0400

    fixed case for zero

--- print_di.c
+++ print_di.c
@@ -1,36 +1,45 @@
 #include "main.h"
 
 /**
- * print_di - Handles the conversion specifiers 'd' and 'i'
- * @args: Variadic argument list containing the integer to print
+ * print_integer - Handles the conversion specifiers 'd' and 'i'
+ * @args: integers to print
  * Return: The number of characters printed
  */
+
 int print_di(va_list args)
 {
 	int n = va_arg(args, int);
 	int count = 0;
 	unsigned int num;
-	char buffer[20];
-
+	char buffer[20]; /* Enough to hold an integer */
 	int i = 0;
 
-	if (n < 0) /* Negative Numbers */
+	if (n < 0)
 	{
-	count += _putchar('-'); /* Prints '-' before negative numbers */
-	num = n; /* Convert to positive for printing */
-	}
+	  count += _putchar('-');
+	  num = -n; /* Convert to positive for printing */
+  }
 	else
 	{
-	num = -n; /* For regular numbers */
-	}
+	  num = n; /* For non-negative, keep num as it is */
+  }
+
+	/* Special case for zero */
+	if (num == 0)
+	{
+	  count += _putchar('0');
+	  return (count);
+  }
+
 	do {
 		buffer[i++] = (num % 10) + '0'; /* Get last digit */
-		num /= 10; /* Remove last difit */
+		num /= 10; /* Remove last digit */
 	} while (num > 0);
-	/* Print digits in correct order */
+
+	/* reverse order of digits */
 	while (i--)
 	{
-	count += _putchar(buffer[i]);
-	}
+	  count += _putchar(buffer[i]);
+  }
 	return (count);
 }
